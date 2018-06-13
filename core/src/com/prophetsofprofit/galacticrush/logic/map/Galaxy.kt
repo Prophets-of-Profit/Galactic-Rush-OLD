c040b3dc6ec291e459ef1ed3d86d3e437d52c44a
Saurabh Totey <SaurabhTotey@gmail.com>
2018-06-13 13:31:26 -0600

    Made extremely basic Galaxy and Planet class

--- core/src/com/prophetsofprofit/galacticrush/logic/map/Galaxy.kt
+++ core/src/com/prophetsofprofit/galacticrush/logic/map/Galaxy.kt
@@ -1,3 +1,21 @@
 package com.prophetsofprofit.galacticrush.logic.map
 
-//TODO: make a galaxy class that is essentially the entire map
+/**
+ * A class that is basically the map that the game is played on
+ * Contains a bunch of planets which are essentially the game 'tiles'
+ * Planets are connected as a graph rather than sequentially
+ */
+class Galaxy(numPlanets: Int) {
+
+    //The planets that are in the galaxy: serve as 'tiles' of the game, but are connected as a graph
+    val planets = Array(numPlanets, { Planet() })
+
+    /**
+     * Galaxy constructor generates all the planets and terrain and values and such
+     * While this isn't a world, a good term for this would be 'worldgen'
+     */
+    init {
+        //TODO: make
+    }
+
+}
--- core/src/com/prophetsofprofit/galacticrush/logic/map/Planet.kt
+++ core/src/com/prophetsofprofit/galacticrush/logic/map/Planet.kt
@@ -1,3 +1,10 @@
 package com.prophetsofprofit.galacticrush.logic.map
 
-//TODO: make a planet class that will populate galaxies and will be the nodes along which things travel
+/**
+ * A class that represents a node in the graph that is the galaxy
+ * Planets are the 'tiles' of the game
+ * Planets can be traversed to and from depending on the edges of the graph, or paths in the galaxy
+ * Planets have arbitrary attributes that are used by other entities in the game
+ * Takes in its location in 2d space
+ */
+class Planet //TODO: make
