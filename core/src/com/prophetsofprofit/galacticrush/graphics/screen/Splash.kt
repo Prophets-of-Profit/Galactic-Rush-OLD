57cffeb8815b4cecd4c519f5893fdd4d4da99a36
Saurabh Totey <SaurabhTotey@gmail.com>
2018-06-22 14:24:16 -0600

    Untested: further improved Splash screen

--- core/src/com/prophetsofprofit/galacticrush/graphics/screen/Splash.kt
+++ core/src/com/prophetsofprofit/galacticrush/graphics/screen/Splash.kt
@@ -6,16 +6,19 @@ import com.badlogic.gdx.graphics.g2d.Sprite
 import com.prophetsofprofit.galacticrush.Main
 import ktx.app.KtxScreen
 import ktx.app.use
+import kotlin.math.min
 
 /**
- * The screen that first shows up that shows the user who made this game
+ * The screen that first shows up that shows the user who made this game (the Prolific Prophets of Profit)
  * Displays for a short time and plays a quick jingle before procceding to the main menu 
  */
 class Splash(val game: Main): KtxScreen {
 
     //How long to wait before going to MainMenu
-    var wait = 5f
-    //The TTS sound that will play on opening
+    val maxWait = 5f
+    //How long the user has waited
+    var currentWait = 0f
+    //The sound that will play on opening
     val sound = Gdx.audio.newSound(Gdx.files.internal("meta/ProphetsOfProfit.ogg"))
     //The prophets of profit logo
     val logo = Sprite(Texture("meta/CapitalMan.png"))
@@ -23,21 +26,22 @@ class Splash(val game: Main): KtxScreen {
     //Initializes all assets
     init {
         this.sound.play()
+        this.game.batch.projectionMatrix = this.game.camera.combined
+        this.logo.setCenter(this.game.camera.viewportWidth / 2, this.game.camera.viewportHeight / 2)
     }
 
     /**
      * Draws the splash screen
-     * Subtracts from wait the amount of time in each render step until zero, then goes to the main menu
-     * Renders the prophets of profits logo
+     * Updates how long the user has waited, and will move to menu screen once user has waited long enough
+     * Renders the prophets of profits logo with increasing visibility with maximum visibility after the screen has appeared for half of its duration
      */
     override fun render(delta: Float) {
-        this.wait -= delta
-        if (this.wait <= 0) {
+        this.currentWait += delta
+        if (this.currentWait >= this.maxWait) {
             this.game.screen = MainMenu(this.game)
         }
+        this.logo.setAlpha(min(1f, 2 * this.currentWait / this.maxWait))
         this.game.batch.use {
-            //Center the logo
-            logo.setPosition(300f, 250f)
             this.logo.draw(it)
         }
     }
