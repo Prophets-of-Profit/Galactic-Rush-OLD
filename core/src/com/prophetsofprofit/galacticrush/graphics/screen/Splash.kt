10fa16daa30d4720742e7836648545d9ce4d79af
Kxoe <kxoethecat@gmail.com>
2018-06-22 12:36:32 -0600

    Improved splash screen

--- core/src/com/prophetsofprofit/galacticrush/graphics/screen/Splash.kt
+++ core/src/com/prophetsofprofit/galacticrush/graphics/screen/Splash.kt
@@ -8,25 +8,27 @@ import ktx.app.KtxScreen
 import ktx.app.use
 
 /**
- * The screen that first shows up that shows the user who made this game (US!)
+ * The screen that first shows up that shows the user who made this game
+ * Displays for a short time and plays a quick jingle before procceding to the main menu 
  */
 class Splash(val game: Main): KtxScreen {
 
     //How long to wait before going to MainMenu
     var wait = 5f
     //The TTS sound that will play on opening
-    val sound = Gdx.audio.newSound(Gdx.files.internal("meta/ProphetsOfProfitTTS.mp3"))
+    val sound = Gdx.audio.newSound(Gdx.files.internal("meta/ProphetsOfProfit.ogg"))
     //The prophets of profit logo
     val logo = Sprite(Texture("meta/CapitalMan.png"))
 
     //Initializes all assets
     init {
         this.sound.play()
-        //TODO: center logo here
     }
 
     /**
-     * TODO: doc
+     * Draws the splash screen
+     * Subtracts from wait the amount of time in each render step until zero, then goes to the main menu
+     * Renders the prophets of profits logo
      */
     override fun render(delta: Float) {
         this.wait -= delta
@@ -34,6 +36,8 @@ class Splash(val game: Main): KtxScreen {
             this.game.screen = MainMenu(this.game)
         }
         this.game.batch.use {
+            //Center the logo
+            logo.setPosition(300f, 250f)
             this.logo.draw(it)
         }
     }
