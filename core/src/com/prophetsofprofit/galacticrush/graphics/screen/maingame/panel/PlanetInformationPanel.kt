ebaa090af62e5243667575ce9a9b13661c6290f6
Saurabh Totey <SaurabhTotey@gmail.com>
2018-08-13 13:20:30 -0600

    Made PlanetInformationPanel actually display the relevant information

--- core/src/com/prophetsofprofit/galacticrush/graphics/screen/maingame/panel/PlanetInformationPanel.kt
+++ core/src/com/prophetsofprofit/galacticrush/graphics/screen/maingame/panel/PlanetInformationPanel.kt
@@ -1,10 +1,14 @@
 package com.prophetsofprofit.galacticrush.graphics.screen.maingame.panel
 
 import com.badlogic.gdx.scenes.scene2d.Action
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.utils.Align
 import com.prophetsofprofit.galacticrush.graphics.Direction
 import com.prophetsofprofit.galacticrush.graphics.Panel
 import com.prophetsofprofit.galacticrush.graphics.screen.maingame.MainGameScreen
+import com.prophetsofprofit.galacticrush.logic.map.Attribute
+import ktx.scene2d.Scene2DSkin
 
 /**
  * The panel that will allow the user to view information about the current planet
@@ -15,6 +19,27 @@ class PlanetInformationPanel(gameScreen: MainGameScreen) : Panel(gameScreen, "Pl
      * Initializes the UI components and their positionings
      */
     init {
+        fun makeLabelForAttribute(attribute: Attribute): Label {
+            return Label("", Scene2DSkin.defaultSkin).also {
+                it.setAlignment(Align.center)
+                it.addAction(object : Action() {
+                    override fun act(delta: Float): Boolean {
+                        it.setText(if (gameScreen.selectedPlanet == null) "" else attribute.stringValue(gameScreen.selectedPlanet!!.attributes[attribute]!!))
+                        return false
+                    }
+                })
+            }
+        }
+        this.add(Image(Scene2DSkin.defaultSkin, "Atmosphere")).pad(10f)
+        this.add(makeLabelForAttribute(Attribute.ATMOSPHERE)).expandX().fillX().padRight(10f).row()
+        this.add(Image(Scene2DSkin.defaultSkin, "Humidity")).pad(10f)
+        this.add(makeLabelForAttribute(Attribute.WATER)).expandX().fillX().padRight(10f).row()
+        this.add(Image(Scene2DSkin.defaultSkin, "Mass")).pad(10f)
+        this.add(makeLabelForAttribute(Attribute.MASS)).expandX().padRight(10f).fillX().row()
+        this.add(Image(Scene2DSkin.defaultSkin, "Solidity")).pad(10f)
+        this.add(makeLabelForAttribute(Attribute.SOLIDITY)).expandX().fillX().padRight(10f).row()
+        this.add(Image(Scene2DSkin.defaultSkin, "Temperature")).pad(10f)
+        this.add(makeLabelForAttribute(Attribute.TEMPERATURE)).expandX().fillX().padRight(10f)
         this.addAction(object : Action() {
             override fun act(delta: Float): Boolean {
                 if (gameScreen.selectedPlanet != null) {
