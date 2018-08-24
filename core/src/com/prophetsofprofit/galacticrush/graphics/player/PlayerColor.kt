68fb73f28861afaece31f12d08369f9dafb4b358
Elia Gorokhovsky <epgorokhovsky01@bvsd.org>
2018-08-23 23:31:31 -0600

    Add an enum class for player colors

---
+++ core/src/com/prophetsofprofit/galacticrush/graphics/player/PlayerColor.kt
@@ -0,0 +1,17 @@
+package com.prophetsofprofit.galacticrush.graphics.player
+
+import com.badlogic.gdx.graphics.Color
+
+/**
+ * All the possible colors the player can choose
+ * TODO: handle different drone sprites, etc.
+ */
+enum class PlayerColor(color: Color) {
+    BLUE(Color.BLUE),
+    CYAN(Color.CYAN),
+    GREEN(Color.GREEN),
+    PINK(Color.PINK),
+    PURPLE(Color.PURPLE),
+    RED(Color.RED),
+    YELLOW(Color.YELLOW)
+}
