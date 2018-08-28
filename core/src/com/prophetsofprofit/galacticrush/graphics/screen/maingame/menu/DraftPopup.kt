ef754a840288427b7a4a281e8ee359634f8ea802
Elia Gorokhovsky <epgorokhovsky01@bvsd.org>
2018-08-28 17:59:29 -0600

    Make drone turn animate before draft starts

--- core/src/com/prophetsofprofit/galacticrush/graphics/screen/maingame/menu/DraftPopup.kt
+++ core/src/com/prophetsofprofit/galacticrush/graphics/screen/maingame/menu/DraftPopup.kt
@@ -62,7 +62,13 @@ class DraftPopup(gameScreen: MainGameScreen) : ModalWindow(gameScreen, "Draft Op
                 if (!canBeUsed) {
                     return false
                 }
-                if (!isVisible && gameScreen.mainGame.phase == GamePhase.DRAFT_PHASE && gameScreen.mainGame.currentDraft[gameScreen.player.id]!!.isNotEmpty() && gameScreen.mainGame.currentDraft[gameScreen.player.id]!! != optionsWhenSubmitted) {
+                if (!isVisible &&
+                        gameScreen.mainGame.phase == GamePhase.DRAFT_PHASE &&
+                        gameScreen.mainGame.currentDraft[gameScreen.player.id]!!.isNotEmpty() &&
+                        gameScreen.mainGame.currentDraft[gameScreen.player.id]!! != optionsWhenSubmitted &&
+                        gameScreen.mainGame.droneTurnChanges.isEmpty() &&
+                        gameScreen.turnAnimationHandler.currentlyMoving.isEmpty()
+                        ) {
                     children.forEach { it.act(0f) }
                     appear(Direction.POP, 1f)
                 } else if (isVisible && gameScreen.mainGame.phase != GamePhase.DRAFT_PHASE) {
