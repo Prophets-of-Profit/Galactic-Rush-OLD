d730583d568138f665aafe574a8b107a0dc7fcb9
Saurabh Totey <SaurabhTotey@gmail.com>
2018-11-30 19:59:12 -0700

    Made buttons for Main Menu

--- core/src/com/prophetsofprofit/galacticrush/graphics/screens/menu/MainMenuScreen.kt
+++ core/src/com/prophetsofprofit/galacticrush/graphics/screens/menu/MainMenuScreen.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.utils.Align
 import com.prophetsofprofit.galacticrush.Main
 import com.prophetsofprofit.galacticrush.graphics.GalacticRushScreen
+import com.prophetsofprofit.galacticrush.graphics.onClick
 import ktx.scene2d.Scene2DSkin
 
 /**
@@ -15,9 +16,32 @@ class MainMenuScreen(main: Main) : GalacticRushScreen(main) {
      * Adds all the menu buttons to the stage
      */
     init {
+        /*
+         * OPTIONS BUTTON
+         */
+        val optionsButton = TextButton("Options", Scene2DSkin.defaultSkin)
+        optionsButton.setSize(this.uiContainer.width * 0.3f, this.uiContainer.height * 0.1f)
+        optionsButton.setPosition(this.uiContainer.width * 0.025f, this.uiContainer.height * 0.025f, Align.bottomLeft)
+        this.uiContainer.addActor(optionsButton)
+        optionsButton.onClick { /* TODO: Make */ }
+
+        /*
+         * JOIN GAME BUTTON
+         */
+        val joinGameButton = TextButton("Join Game", Scene2DSkin.defaultSkin)
+        joinGameButton.setSize(this.uiContainer.width * 0.3f, this.uiContainer.height * 0.1f)
+        joinGameButton.setPosition(this.uiContainer.width * 0.025f, optionsButton.top + this.uiContainer.height * 0.025f, Align.bottomLeft)
+        this.uiContainer.addActor(joinGameButton)
+        joinGameButton.onClick { /* TODO: Make */ }
+
+        /*
+         * HOST GAME BUTTON
+         */
         val hostGameButton = TextButton("Host Game", Scene2DSkin.defaultSkin)
-        hostGameButton.setPosition(100f, 100f, Align.center)
+        hostGameButton.setSize(this.uiContainer.width * 0.3f, this.uiContainer.height * 0.1f)
+        hostGameButton.setPosition(this.uiContainer.width * 0.025f, joinGameButton.top + this.uiContainer.height * 0.025f, Align.bottomLeft)
         this.uiContainer.addActor(hostGameButton)
+        hostGameButton.onClick { /* TODO: Make */ }
     }
 
     /**
